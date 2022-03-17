6de64d02487798b031fcea388fe88913cc91ddb4
Kenta Kubo <kkk669@users.noreply.github.com>
2022-03-17 23:01:59 +0900

    Format

--- Sources/DevToysApp/ViewModels/EncodersDecoders/HTMLEncoderDecoderViewModel.swift
+++ Sources/DevToysApp/ViewModels/EncodersDecoders/HTMLEncoderDecoderViewModel.swift
@@ -5,22 +5,22 @@ final class HTMLEncoderDecoderViewModel {
     @Published var encodeMode = true
     @Published var input = ""
     @Published var output = ""
-    
+
     init() {
         self.$input.combineLatest(self.$encodeMode)
             .dropFirst()
             .map { input, encodeMode in
                 encodeMode
-                ? Self.encode(input)
-                : Self.decode(input)
+                    ? Self.encode(input)
+                    : Self.decode(input)
             }
             .assign(to: &self.$output)
     }
-    
+
     private static func encode(_ input: String) -> String {
         input.htmlEscape(useNamedReferences: true)
     }
-    
+
     private static func decode(_ input: String) -> String {
         input.htmlUnescape()
     }
