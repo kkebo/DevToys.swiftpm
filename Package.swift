ee225a6a7ab4cc56f209cd511fc3f6f8e501d0d0
Kenta Kubo <kkk669@users.noreply.github.com>
2022-04-24 00:48:00 +0900

    Set ITSAppUsesNonExemptEncryption to NO

--- Package.swift
+++ Package.swift
@@ -31,7 +31,8 @@ let package = Package(
                 .landscapeRight,
                 .landscapeLeft,
                 .portraitUpsideDown(.when(deviceFamilies: [.pad]))
-            ]
+            ],
+            additionalInfoPlistContentFilePath: "Info.plist"
         )
     ],
     dependencies: [
