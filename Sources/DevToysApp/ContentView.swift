152ce7ea3a68d3c754a8dc82c74541a571235e4c
Kenta Kubo <kkk669@users.noreply.github.com>
2022-10-17 09:06:28 +0900

    Always show the sidebar

--- Sources/DevToysApp/ContentView.swift
+++ Sources/DevToysApp/ContentView.swift
@@ -4,11 +4,12 @@ struct ContentView {
     @StateObject private var state = AppState()
     @SceneStorage("selectedTool") private var selection: Tool?
     @State private var searchQuery = ""
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
 }
 
 extension ContentView: View {
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView(columnVisibility: self.$columnVisibility) {
             Sidebar(state: self.state, selection: self.$selection, searchQuery: self.$searchQuery)
         } detail: {
             if let tool = self.selection {
@@ -17,6 +18,7 @@ extension ContentView: View {
                 AllToolsView(state: self.state, searchQuery: self.$searchQuery)
             }
         }
+        .navigationSplitViewStyle(.balanced)
         .onContinueUserActivity("xyz.kebo.DevToysForiPad.newWindow") {
             let payload = try? $0.typedPayload(NewWindowActivityPayload.self)
             self.selection = payload?.tool
