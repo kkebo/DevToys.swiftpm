b743e93b6e2e1c37e9402b414511cbcbc742d4bc
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-01-20 01:22:17 +0900

    build: use my fork
    until mchakravarty/CodeEditorView#96 is merged

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/lukaskubanek/LoremSwiftum", from: "2.2.1"),
         .package(url: "https://github.com/JohnSundell/Ink", from: "0.6.0"),
         .package(url: "https://github.com/kkk669/swift-uniyaml", branch: "origin/issues/1"),
-        .package(url: "https://github.com/mchakravarty/CodeEditorView", branch: "main"),
+        .package(url: "https://github.com/kkk669/CodeEditorView", branch: "origin/fix-updating-font"),
     ],
     targets: [
         .executableTarget(
