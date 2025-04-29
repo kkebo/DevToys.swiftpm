25132a312c84109d0597e752469d6b1b2499ef2f
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-04-30 01:39:32 +0900

    fix: use swift-log-playground only if running on Swift Playground or Xcode Previews

--- Sources/DevToysApp.swift
+++ Sources/DevToysApp.swift
@@ -7,7 +7,10 @@ let logger = Logger(label: "main")
 @main
 struct DevToysApp {
     init() {
-        LoggingSystem.bootstrap { PlaygroundHandler(label: $0) }
+        // Use swift-log-playground only if running on Swift Playground or Xcode Previews
+        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+            LoggingSystem.bootstrap { PlaygroundHandler(label: $0) }
+        }
     }
 }
 
