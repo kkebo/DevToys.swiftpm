78924e04c2f66933237a60c0dcd6ff5c9d2afcbc
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-09-07 00:43:31 +0900

    fix: fix previews on iPadOS 26
    fixes #148

--- Sources/Extensions/View+previewPresets.swift
+++ Sources/Extensions/View+previewPresets.swift
@@ -1,35 +1,25 @@
 import SwiftUI
 
-struct PreviewPresets {
-    static var dynamicTypeSizes: [DynamicTypeSize] {
+extension View {
+    private static var dynamicTypeSizes: [DynamicTypeSize] {
         [
             .xSmall,
             .medium,
             .xxxLarge,
         ]
     }
-}
 
-extension PreviewPresets: ViewModifier {
-    func body(content: Self.Content) -> some View {
+    func previewPresets() -> some View {
         Group {
             ForEach(ColorScheme.allCases, id: \.self) { scheme in
-                content
-                    .preferredColorScheme(scheme)
+                self.preferredColorScheme(scheme)
                     .previewDisplayName("\(scheme)")
             }
 
             ForEach(Self.dynamicTypeSizes, id: \.self) { size in
-                content
-                    .environment(\.dynamicTypeSize, size)
+                self.environment(\.dynamicTypeSize, size)
                     .previewDisplayName("\(size)")
             }
         }
-    }
-}
-
-extension View {
-    func previewPresets() -> some View {
-        self.modifier(PreviewPresets())
     }
 }
