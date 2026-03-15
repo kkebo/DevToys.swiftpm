ad533d82e53544e22ff4230957c1ab5f3b2e7653
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-03-15 21:05:26 +0900

    chore: bump version to 1.0.0 (29)

--- Package.swift
+++ Package.swift
@@ -66,7 +66,7 @@ let package = Package(
             bundleIdentifier: "xyz.kebo.DevToysForiPad",
             teamIdentifier: "X4678G5DL2",
             displayVersion: "1.0.0",
-            bundleVersion: "28",
+            bundleVersion: "29",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.purple),
             supportedDeviceFamilies: [
